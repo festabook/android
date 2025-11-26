86cc75b177ed80db9cc8eb4eae1d6c60036a5fc9
박지민 <pjm5489@naver.com>
2025-11-26 21:28:13 +0900

    feat: Header 컴포저블 프리뷰 구현

--- app/src/main/java/com/daedan/festabook/presentation/common/component/Header.kt
+++ app/src/main/java/com/daedan/festabook/presentation/common/component/Header.kt
@@ -1,10 +1,12 @@
 package com.daedan.festabook.presentation.common.component
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daedan.festabook.presentation.theme.FestabookTypography
 
@@ -17,6 +19,15 @@ fun Header(
     Text(
         text = title,
         style = style,
-        modifier = modifier.padding(top = 40.dp, bottom = 16.dp, start = 16.dp, end = 16.dp),
+        modifier =
+            modifier
+                .padding(top = 40.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
     )
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun HeaderPreview() {
+    Header(title = "FestaBook")
 }
