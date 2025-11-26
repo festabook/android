94c1993e500d7cba3f09fdcbda9f0ce9e821e840
박지민 <pjm5489@naver.com>
2025-11-26 20:22:19 +0900

    feat: 공통 헤더 구현

---
+++ app/src/main/java/com/daedan/festabook/presentation/common/component/Header.kt
@@ -0,0 +1,22 @@
+package com.daedan.festabook.presentation.common.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.daedan.festabook.presentation.theme.FestabookTypography
+
+@Composable
+fun Header(
+    title: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = FestabookTypography.displayLarge,
+) {
+    Text(
+        text = title,
+        style = style,
+        modifier = modifier.padding(top = 40.dp, bottom = 16.dp, start = 16.dp, end = 16.dp),
+    )
+}
