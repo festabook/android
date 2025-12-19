bb90581e4fbe0cab2e797c90bea894c8ea3e5dd7
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-12-19 23:51:11 +0900

    feat(home): HomeLineupHeader 컴포저블 추가

---
+++ app/src/main/java/com/daedan/festabook/presentation/home/component/HomeLineupHeader.kt
@@ -0,0 +1,78 @@
+package com.daedan.festabook.presentation.home.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.daedan.festabook.R
+import com.daedan.festabook.presentation.theme.FestabookColor
+import com.daedan.festabook.presentation.theme.FestabookTypography
+
+@Composable
+fun HomeLineupHeader(
+    onScheduleClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(R.string.home_lineup_title),
+            style = FestabookTypography.displayMedium,
+            color = FestabookColor.black,
+        )
+
+        Row(
+            modifier =
+                Modifier
+                    .clickable(
+                        onClick = onScheduleClick,
+                    )
+                    .padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = stringResource(R.string.home_check_schedule_text),
+                style = FestabookTypography.bodySmall,
+                color = FestabookColor.gray400,
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_forward_right),
+                contentDescription = null,
+                tint = FestabookColor.gray400,
+                modifier = Modifier.size(12.dp),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeLineupHeaderPreview() {
+    HomeLineupHeader(
+        onScheduleClick = {},
+    )
+}
