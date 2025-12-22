401b44bdafbc5ee2ec9d9030fb85015b041fa677
박지민 <pjm5489@naver.com>
2025-12-22 21:46:37 +0900

    refactor: ScheduleEventItem 코드 구조 개선 및 가독성 향상

--- app/src/main/java/com/daedan/festabook/presentation/schedule/component/ScheduleEventItem.kt
+++ app/src/main/java/com/daedan/festabook/presentation/schedule/component/ScheduleEventItem.kt
@@ -25,74 +25,17 @@ fun ScheduleEventItem(
     scheduleEvent: ScheduleEventUiModel,
     modifier: Modifier = Modifier,
 ) {
-    val props =
-        when (scheduleEvent.status) {
-            ScheduleEventUiStatus.UPCOMING -> {
-                LottieTimeLineCircleProps(
-                    centerColor = FestabookColor.accentGreen,
-                    outerOpacity = 0f,
-                    innerOpacity = 1f,
-                    outerColor = FestabookColor.accentGreen,
-                    innerColor = FestabookColor.accentGreen,
-                )
-            }
-
-            ScheduleEventUiStatus.ONGOING -> {
-                LottieTimeLineCircleProps(
-                    centerColor = FestabookColor.accentBlue,
-                    outerOpacity = 1f,
-                    innerOpacity = 1f,
-                    outerColor = FestabookColor.accentBlue,
-                    innerColor = FestabookColor.accentBlue,
-                )
-            }
-
-            ScheduleEventUiStatus.COMPLETED -> {
-                LottieTimeLineCircleProps(
-                    centerColor = FestabookColor.gray300,
-                    outerOpacity = 0f,
-                    innerOpacity = 0f,
-                    outerColor = FestabookColor.gray300,
-                    innerColor = FestabookColor.gray300,
-                )
-            }
-        }
-    val dynamicProperties =
-        rememberLottieDynamicProperties(
-            rememberLottieDynamicProperty(
-                property = LottieProperty.COLOR,
-                value = props.centerColor.toArgb(),
-                *props.centerKeyPath.toTypedArray(),
-            ),
-            rememberLottieDynamicProperty(
-                property = LottieProperty.OPACITY,
-                value = (props.outerOpacity * 100).toInt(),
-                *props.outerKeyPath.toTypedArray(),
-            ),
-            rememberLottieDynamicProperty(
-                property = LottieProperty.OPACITY,
-                value = (props.innerOpacity * 100).toInt(),
-                *props.innerKeyPath.toTypedArray(),
-            ),
-            rememberLottieDynamicProperty(
-                property = LottieProperty.COLOR,
-                value = props.outerColor.toArgb(),
-                *props.outerKeyPath.toTypedArray(),
-            ),
-            rememberLottieDynamicProperty(
-                property = LottieProperty.COLOR,
-                value = props.innerColor.toArgb(),
-                *props.innerKeyPath.toTypedArray(),
-            ),
-        )
+    val props = lottieTimeLineCircleProps(scheduleEvent.status)
+    val dynamicProperties = rememberScheduleEventDynamicProperties(props)
 
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         LottieAnimation(
             composition = composition,
-            progress = { progress },
-            dynamicProperties = dynamicProperties,
+            progress = { if (scheduleEvent.status == ScheduleEventUiStatus.COMPLETED) 0f else progress },
             modifier = Modifier.size(festabookSpacing.paddingBody4 * 4),
+            dynamicProperties = dynamicProperties,
         )
+
         ScheduleEventCard(scheduleEvent = scheduleEvent)
     }
 }
@@ -113,6 +56,70 @@ private fun ScheduleEventItemPreview() {
                 location = "운동장",
             ),
     )
+}
+
+@Composable
+private fun rememberScheduleEventDynamicProperties(props: LottieTimeLineCircleProps) =
+    rememberLottieDynamicProperties(
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR,
+            value = props.centerColor.toArgb(),
+            *props.centerKeyPath.toTypedArray(),
+        ),
+        rememberLottieDynamicProperty(
+            property = LottieProperty.OPACITY,
+            value = (props.outerOpacity * 100).toInt(),
+            *props.outerKeyPath.toTypedArray(),
+        ),
+        rememberLottieDynamicProperty(
+            property = LottieProperty.OPACITY,
+            value = (props.innerOpacity * 100).toInt(),
+            *props.innerKeyPath.toTypedArray(),
+        ),
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR,
+            value = props.outerColor.toArgb(),
+            *props.outerKeyPath.toTypedArray(),
+        ),
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR,
+            value = props.innerColor.toArgb(),
+            *props.innerKeyPath.toTypedArray(),
+        ),
+    )
+
+private fun lottieTimeLineCircleProps(status: ScheduleEventUiStatus): LottieTimeLineCircleProps =
+    when (status) {
+        ScheduleEventUiStatus.UPCOMING -> LottieTimeLineCircleAnimations.upcoming
+        ScheduleEventUiStatus.ONGOING -> LottieTimeLineCircleAnimations.ongoing
+        ScheduleEventUiStatus.COMPLETED -> LottieTimeLineCircleAnimations.completed
+    }
+
+object LottieTimeLineCircleAnimations {
+    val upcoming =
+        LottieTimeLineCircleProps(
+            centerColor = FestabookColor.accentGreen,
+            outerOpacity = 0f,
+            innerOpacity = 1f,
+            outerColor = FestabookColor.accentGreen,
+            innerColor = FestabookColor.accentGreen,
+        )
+    val ongoing =
+        LottieTimeLineCircleProps(
+            centerColor = FestabookColor.accentBlue,
+            outerOpacity = 1f,
+            innerOpacity = 1f,
+            outerColor = FestabookColor.accentBlue,
+            innerColor = FestabookColor.accentBlue,
+        )
+    val completed =
+        LottieTimeLineCircleProps(
+            centerColor = FestabookColor.gray300,
+            outerOpacity = 0f,
+            innerOpacity = 0f,
+            outerColor = FestabookColor.gray300,
+            innerColor = FestabookColor.gray300,
+        )
 }
 
 data class LottieTimeLineCircleProps(
