882f58ddcd635e710252814b058f596689315f83
박지민 <pjm5489@naver.com>
2026-01-10 00:39:43 +0900

    refactor:Context 참조 방식 변경

--- app/src/main/java/com/daedan/festabook/presentation/common/component/CoilImage.kt
+++ app/src/main/java/com/daedan/festabook/presentation/common/component/CoilImage.kt
@@ -5,9 +5,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
 import com.daedan.festabook.presentation.common.convertImageUrl
 
@@ -22,7 +22,7 @@ fun CoilImage(
     AsyncImage(
         model =
             ImageRequest
-                .Builder(LocalContext.current)
+                .Builder(LocalPlatformContext.current)
                 .apply(builder)
                 .data(url.convertImageUrl())
                 .build(),
