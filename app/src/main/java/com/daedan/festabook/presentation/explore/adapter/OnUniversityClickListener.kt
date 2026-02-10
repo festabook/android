ca461dfc49df07f8c1b13837cfda0f718a7396b3
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2026-02-10 11:15:30 +0900

    [Feat] 탐색화면 Compose 마이그레이션 (#50)
    * feat(Explore): 검색 바 및 결과 아이템 컴포저블 추가
    
    * feat(Explore): 축제 탐색 화면 UI 컴포저블 구현
    
    * refactor(Explore): 탐색 화면 UI 및 로직 개선
    
    - 검색 결과 목록을 `ExploreSearchResultList` 컴포저블로 분리했습니다.
    - 검색어 유무에 따라 검색창과 로고의 위치가 애니메이션과 함께 동적으로 변경되도록 수정했습니다.
    - 검색 실행 시 키보드가 자동으로 숨겨지도록 구현했습니다.
    - 화면 전체적인 레이아웃과 패딩을 조정하여 UI를 개선했습니다.
    
    * refactor(Explore): 화면을 Compose로 전환하고 로직 개선
    
    * refactor(Explore): 검색 화면 전환 시 애니메이션 개선
    
    * refactor(Explore): ViewModel의 Channel을 SharedFlow로 변경
    
    * refactor(Explore): ktilnt에 맞게 리팩터링
    
    * refactor(Explore): 코드 개선 및 스트링 리소스 적용
    
    - `onSearch` 람다 호출 시 `query`를 전달하도록 수정했습니다.
    - 하드코딩된 "뒤로가기"와 로고의 `contentDescription`을 스트링 리소스로 대체했습니다.
    
    * refactor(Explore): 불필요한 코드 제거 및 패딩 수정
    
    * refactor(Explore): 탐색 화면 UI 개선
    
    - `BoxWithConstraints`를 `Box`와 `weight`로 변경하여 랜딩 화면의 레이아웃을 보다 명확하게 개선했습니다.
    
    * refactor(Explore): Flow 수집 방식 개선
    
    * refactor(Explore): SearchUiState 리팩터링 및 UI 상태 로직 개선
    
    - `SearchUiState`를 sealed interface로 변경하고, UI 상태를 더 명확하게 표현하기 위해 `isEmptyResult`, `isFailure`, `shouldShowErrorUi` 프로퍼티를 추가했습니다.
    - `ExploreSearchContent`와 `ExploreScreen`에서 `shouldShowErrorUi` 프로퍼티를 사용하여 에러 상태 UI를 결정하도록 로직을 단순화했습니다.
    
    * refactor(Explore): 검색창 컴포저블 색상 값 수정
    
    - `ExploreSearchBar`의 컨테이너 색상을 투명(`Transparent`)으로 변경했습니다.
    - 검색창의 커서 및 비활성화 상태의 테두리 색상 값을 추가했습니다.
    
    * refactor(Explore): ExploreViewModel 테스트 코드 개선

--- app/src/main/java/com/daedan/festabook/presentation/explore/adapter/OnUniversityClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.daedan.festabook.presentation.explore.adapter
-
-import com.daedan.festabook.presentation.explore.model.SearchResultUiModel
-
-fun interface OnUniversityClickListener {
-    fun onUniversityClick(university: SearchResultUiModel)
-}
